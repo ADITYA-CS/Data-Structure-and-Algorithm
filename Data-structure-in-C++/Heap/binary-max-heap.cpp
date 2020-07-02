5d5810576e5fa570d75ef7f47ef2f94b543e59ab
Aditya Kumar Singh <cr7.aditya@gmail.com>
2020-07-02 20:59:38 +0530

    Add: Binary Max heap Modified: Binary Min heap
    changes the variable and function name, completed BubbleDown todo
    Didn't add any check for error when popping and pushing when heap is
    empty or full.

---
+++ Data-structure-in-C++/Heap/binary-max-heap.cpp
@@ -0,0 +1,152 @@
+/**
+ * @author : Aditya Kumar Singh
+ * Max heap
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class MaxHeap{
+private:
+    vector<int> heap_;
+    int max_size_; // maximum size of heap
+    int size_; // current size of heap
+
+private:
+    int Parent(int);
+    int LeftChild(int);
+    int RightChild(int);
+    void BubbleUp(int);
+    void BubbleDown(int);
+
+public:
+    explicit MaxHeap(int n);
+    void Push(int); // pushing an element when heap is  full,
+                    // may produce undefined behaviour
+    int Pop();      // poping an element when heap is empty,
+                    // may produce undefined behaviour
+    int Top();
+    bool Full();
+    bool Empty();
+};
+int main() {
+    MaxHeap heap(10);
+    heap.Push(5);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(4);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(7);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(1);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Pop();
+    cout << "Top : " << heap.Top() << "\n";
+}
+
+MaxHeap::MaxHeap(int size) : max_size_(size), size_(0) {
+    heap_.resize(size);
+}
+
+/**
+ *  return parent node of the child node
+ *  parent node index = floor(node index / 2);
+ */
+int MaxHeap::Parent(int child) {
+    return child / 2;
+}
+
+/**
+ *  return left child node index of the node
+ *  left child index = 2 * parent node index
+ */
+int MaxHeap::LeftChild(int parent) {
+    return parent * 2;
+}
+
+/**
+ * return right child node index of the node
+ * right child index = 2 * parent node index + 1
+ */
+int MaxHeap::RightChild(int parent) {
+    return parent * 2 + 1;
+}
+
+/**
+ * moves the element towards the root and place it at right place
+ */
+void MaxHeap::BubbleUp(int index) {
+    int parent = Parent(index);
+    while (index > 1 && heap_[parent] < heap_[index]) {
+        swap(heap_[parent], heap_[index]);
+        index = parent;
+        parent = Parent(index);
+    }
+}
+
+/**
+ * moves the element towards the bottom and place it at right place
+ */
+void MaxHeap::BubbleDown(int index) {
+    int left = LeftChild(index);
+    int right = RightChild(index);
+    int max_index = index;
+
+    if (left <= size_ && heap_[left] > heap_[max_index])
+        max_index = left;
+
+    if (right <= size_ && heap_[right] > heap_[max_index])
+        max_index = right;
+
+    if (index != max_index) { // base case
+        swap(heap_[index], heap_[max_index]);
+        BubbleDown(max_index);
+    }
+}
+
+/**
+ *  push an element in heap
+ */
+void MaxHeap::Push(int el) {
+    if (Full()) {
+        return;
+    }
+    size_++;
+    heap_[size_] = el;
+    BubbleUp(size_);
+}
+
+/**
+ * pop the element with highest value
+ */
+int MaxHeap::Pop(){
+    if (Empty())
+        return;
+    int result = heap_[1];
+    heap_[1] = heap_[size_];
+    size_--;
+    BubbleDown(1);
+    return result;
+}
+
+/**
+ * return the element with highest value
+ */
+int MaxHeap::Top() {
+    return heap_[1];
+}
+
+/**
+ * check heap if full or not
+ */
+bool MaxHeap::Full() {
+    return size_ == max_size_ - 1;
+}
+
+/**
+ * check heap if empty or not
+ */
+bool MaxHeap::Empty() {
+    return size_ == 0;
+}
--- Data-structure-in-C++/Heap/binary-min-heap.cpp
+++ Data-structure-in-C++/Heap/binary-min-heap.cpp
@@ -5,155 +5,157 @@
 
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 
 class MinHeap{
 // private data
 private:
-    int *heap_; // pointer an array
-    int capacity_;   // memory allocated to heap_
-    int current_size_; // keep record of last element in heap_ array
+    vector<int> heap_; // pointer an array
+    int max_size_;   // memory allocated to heap_
+    int size_; // keep record of last element in heap_ array
+
 //private function
 private:
     int Parent(int) const;
-    int Left(int) const;
-    int Right(int) const;
+    int LeftChild(int) const;
+    int RightChild(int) const;
     void BubbleUp(int);
     void BubbleDown(int);
+
 //public function
 public:
-    MinHeap();
-    MinHeap(int);
-    MinHeap(int *, int);
-    int get_min() const;
+    explicit MinHeap(int);
+    int Top() const;
     int extract();
-    void Insert(int);
-    void Delete();
+    void Push(int);
+    int Pop();
     bool Full() const;
     bool Empty() const;
     void Display();
 };
 
 
-int main(){
-    MinHeap mh(5);
-    mh.Insert(40);
-    mh.Display();
-    mh.Insert(30);
-    mh.Display();
-    mh.Insert(60);
-    mh.Display();
-    mh.Insert(20);
-    mh.Display();
-    mh.Insert(10);
-    mh.Display();
-    cout << " Min : " << mh.get_min() << "\n";
-    mh.Display();
-    cout << " Min : " << mh.extract() << "\n";
-    mh.Display();
+int main() {
+    MinHeap heap(5);
+    heap.Push(40);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(30);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(60);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(20);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Push(10);
+    cout << "Top : " << heap.Top() << "\n";
+    heap.Pop();
+    cout << "Top : " << heap.Top() << "\n";
 }
 
-MinHeap :: MinHeap() : capacity_(100), current_size_(-1){
-    heap_ = new int[100];
+MinHeap::MinHeap(int size) : max_size_(size + 1), size_(0) {
+    heap_.resize(size);
 }
 
-MinHeap :: MinHeap(int n) : capacity_(n), current_size_(-1){
-    heap_ = new int[n];
+/**
+ *  return parent node of the child node
+ *  parent node index = floor(node index / 2);
+ */
+int MinHeap::Parent(int child) const {
+    return child / 2;
 }
-// Todo
-MinHeap :: MinHeap(int *arr, int n) : capacity_(n), current_size_(n-1){
-    heap_ = new int[n];
-    for(int i = 0; i < n; i++){
-        heap_[i] = arr[i];
+
+/**
+ *  return left child node index of the node
+ *  left child index = 2 * parent node index
+ */
+int MinHeap::LeftChild(int parent) const {
+    return 2 * parent;
+}
+
+/**
+ * return right child node index of the node
+ * right child index = 2 * parent node index + 1
+ */
+int MinHeap::RightChild(int parent) const {
+    return 2 * parent + 1;
+}
+
+/**
+ * check heap if full or not
+ */
+bool MinHeap::Full() const {
+    return size_ == max_size_ - 1;
+}
+
+/**
+ * check heap if empty or not
+ */
+bool MinHeap::Empty() const {
+    return size_ == 0;
+}
+
+/**
+ *  push an element in heap
+ */
+void MinHeap::Push(int el) {
+    if (Full()) {
+        return;
     }
-    // Now building min heap
-    int index = Parent(current_size_);
-    while(index >= 0){
-        BubbleDown(index);
-    }
 
+    size_++;
+    heap_[size_] = el;
+    BubbleUp(size_);
 }
 
-int MinHeap :: Parent(int i) const{
-    return i > 0 ? (i - 1) / 2 : -1;
-}
-int MinHeap :: Left(int i) const{
-    return 2 * i + 1;
-}
-int MinHeap :: Right(int i) const{
-    return 2 * i + 2;
+
+/**
+ * pop the element with lowest value
+ */
+int MinHeap::Pop() {
+    int result = heap_[1];
+    heap_[1] = heap_[size_];
+    size_--;
+    BubbleDown(1);
+    return result;
 }
 
-bool MinHeap :: Full() const{
-    return current_size_ == capacity_ - 1;
+/**
+ * return the element with lowest value
+ */
+int MinHeap::Top() const {
+    return heap_[1];
 }
 
-bool MinHeap :: Empty() const{
-    return current_size_ == -1;
+/**
+ * moves the element towards the root and place it at right place
+ */
+void MinHeap::BubbleUp(int index) {
+    int parent = Parent(index);
+    while (index > 1 && heap_[parent] > heap_[index]) {
+        swap(heap_[parent], heap_[index]);
+        index = parent;
+        parent = Parent(index);
+    }
 }
 
-void MinHeap :: Insert(int el){
-    if(Full()){
-        cerr << "Heap is full" << '\n';
-        exit(1);
-    }
-    current_size_++;
-    heap_[current_size_] = el;
-    BubbleUp(current_size_);
-}
+/**
+ * moves the element towards the bottom and place it at right place
+ */
+void MinHeap::BubbleDown(int index) {
+    int left = LeftChild(index);
+    int right = RightChild(index);
+    int min_index = index;
 
-void MinHeap :: BubbleUp(int index){
-    int temp = heap_[index];
-    int par = Parent(index);
-    while(par >= 0 && heap_[par] > temp){
-        heap_[index] = heap_[par];
-        index = par;
-        par = Parent(index);
-    }
-    heap_[index] = temp;
-}
+    if (left <= size_ && heap_[left] < heap_[index])
+        min_index = left;
 
-void MinHeap :: BubbleDown(int index){
-    int left, right, smallest;
-    while(true){
-        left = Left(index);
-        right = Right(index);
-        smallest = index;
-        if(left <= current_size_ && heap_[left] < heap_[smallest]){
-            smallest = left;
-        }
-        if(right <= current_size_ && heap_[right] < heap_[smallest]){
-            smallest = right;
-        }
-        if(smallest == index){
-            break;
-        }
-        swap(heap_[index],heap_[smallest]);
-        index = smallest;
+    if (right <= size_ && heap_[right] < heap_[min_index])
+        min_index = right;
+
+    if (index != min_index) { // base case
+        swap(heap_[index], heap_[min_index]);
+        BubbleDown(min_index);
     }
-}
-int MinHeap :: get_min() const{
-    if(Empty()){
-        cerr << "Heap is empty\n";
-        exit(1);
-    }
-    return heap_[0];
-}
-int MinHeap :: extract(){
-    if(Empty()){
-        cerr << "Heap is empty\n";
-        exit(1);
-    }
-    int res = heap_[0];
-    heap_[0] = heap_[current_size_];
-    current_size_--;
-    BubbleDown(0);
-    return res;
-}
-void MinHeap :: Display(){
-    for(int i = 0; i <= current_size_; i++){
-        cout << heap_[i] << " ";
-    }
-    cout << "\n";
 }
